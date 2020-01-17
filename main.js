f48fe5b4720cfa2c6597be06a941987c56aa7739
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-17 09:43:38 +0100

    Added working hamburger menu

---
+++ main.js
@@ -0,0 +1,7 @@
+$('i.fas.fa-bars').click(function() {
+  $('div.hamburger-menu').addClass('active');
+});
+
+$('i.fas.fa-times').click(function() {
+  $('div.hamburger-menu').removeClass('active');
+});
